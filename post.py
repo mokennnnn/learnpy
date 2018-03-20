f2c3fcb018ebfc3555e8f56bef72bd724d1561c2
moken <mokenself@163.com>
2018-03-20 15:49:13 +0800

    fix

--- post.py
+++ post.py
@@ -10,6 +10,8 @@ import hashlib
 
 
 # 评论
+glo=0
+
 def send(s):
     headers = {'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_2_6 like Mac OS X) AppleWebKit/604.5.6 (KHTML, like Gecko) Mobile/15D100 MicroMessenger/6.6.5 NetType/WIFI Language/zh_CN'}
     cookies = dict(PHPSESSID='8gah7qc84q7k0eb4uug5j1t8a2')
@@ -19,10 +21,11 @@ def send(s):
     try:
         hl = hashlib.md5();
         hl.update(str(s).encode(encoding='utf-8'));
-        req=print('建立网站链接 %s' %(s));
-        data = {'md5':str(s),'key':hl.hexdigest()};
+        req=print('建立网站链接 %s' %(glo));
+        data = {'md5':str(glo),'key':hl.hexdigest()};
         req = requests.post(url,cookies=cookies,headers=headers,data=data)
         back=req.text;
+        glo=back;
         time.sleep(0.001);
     except:
         back = 'error'
