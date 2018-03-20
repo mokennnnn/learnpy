0cab40b6afe452a731f7cabb50c527b0365adf86
moken <mokenself@163.com>
2018-03-20 15:25:30 +0800

    fix

--- post.py
+++ post.py
@@ -13,11 +13,11 @@ def send(s):
     headers = {'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_2_6 like Mac OS X) AppleWebKit/604.5.6 (KHTML, like Gecko) Mobile/15D100 MicroMessenger/6.6.5 NetType/WIFI Language/zh_CN'}
     cookies = dict(PHPSESSID='8gah7qc84q7k0eb4uug5j1t8a2')
 
-    url = 'http://112.74.179.199:3030/api/registercode'
+    url = 'http://112.74.179.199:3030/api/reg'
 
     try:
         req=print('建立网站链接 %s' %(s));
-        data = {'phone': str(s)};
+        data = {'account':str(s),'password':'123123'};
         req = requests.post(url,cookies=cookies,headers=headers,data=data)
         back=req.text;
         time.sleep(1);
@@ -28,8 +28,8 @@ def send(s):
 
 
 
-for i in range(7440, 7450):
+for i in range(10000, 100000):
     print(i)
 
-    ww=send(str(18819460000+i));
+    ww=send(str(i));
     print(ww);
